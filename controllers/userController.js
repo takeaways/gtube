fa443f2020c819174f5a16c1839482f4686e7e41
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2019-12-29 13:38:38 +0900

    추가 컨트롤러

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,5 @@ export const logout = (req, res) => res.send('logout');
 
 export const users = (req, res) => res.send('users');
 export const userDetail = (req, res) => res.send('userDetail');
-export const userProfile = (req, res) => res.send('userProfile');
 export const editProfile = (req, res) => res.send('editProfile');
 export const changePassword = (req, res) => res.send('changePassword');
