fc0ce43333d930d12dd3d6ace085eff6d9cb6f43
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2019-12-29 15:05:00 +0900

    login editprofile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,21 @@
-export const login = (req, res) => res.send('login');
-export const join = (req, res) => res.send('join');
+export const login = (req, res) => {
+  res.render('login', {
+    pageTitle: '로그인'
+  });
+};
+
+export const join = (req, res) => {
+  res.render('join', {
+    pageTitle: '회원가입'
+  });
+};
 export const logout = (req, res) => res.send('logout');
 
 export const users = (req, res) => res.send('users');
+
+export const editProfile = (req, res) =>
+  res.render('editProfile', {
+    pageTitle: '프로필 수정'
+  });
+export const changePassword = (req, res) => res.render('changePassword');
 export const userDetail = (req, res) => res.send('userDetail');
-export const editProfile = (req, res) => res.send('editProfile');
-export const changePassword = (req, res) => res.send('changePassword');
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import routes from '../routes';
+import { editProfile, changePassword } from '../controllers/userController';
 
 const router = express.Router();
 
-router.get(routes.users, (req, res) => console.log('video users'));
-router.get(routes.userDetail, (req, res) => console.log('video userDetail'));
-router.get(routes.editProfile, (req, res) => console.log('video editProfile'));
-router.get(routes.changePassword, (req, res) =>
-  console.log('video changePassword')
-);
+router.get(routes.editProfile, editProfile);
+router.get(routes.changePassword, changePassword);
+
+router.get(routes.users, (req, res) => console.log('users users'));
+router.get(routes.userDetail, (req, res) => console.log('users userDetail'));
 
 export default router;
